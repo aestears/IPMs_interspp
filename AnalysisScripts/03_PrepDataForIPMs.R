64b6eac44cfd43ed6a6683d555668824ad745ff8
aestears <astears@uwyo.edu>
2023-04-13 13:38:39 -0600

    starting very basic STAN models for vital rate functions

--- AnalysisScripts/03_PrepDataForIPMs.R
+++ AnalysisScripts/03_PrepDataForIPMs.R
@@ -9,6 +9,10 @@ library(tidyverse)
 library(plantTracker)
 library(sf)
 library(mapview)
+library(rstan)
+
+# Load data from previous script ------------------------------------------
+source("./AnalysisScripts/02_RecruitData.R")
 
 # decide which species to use ---------------------------------------------
 # get Hesperostipa comata species from three sites into one data.frame
@@ -22,8 +26,8 @@ hesCom <- rbind(MT_hesCom, CO_hesCom, ID_hesCom)
 hesCom$Species <- "Hesperostipa comata"
 
 # visualize survival and growth for this species
-
-survMod <- glm(survives_tplus1 ~ basalArea_genet + age, data = hesCom, family = "binomial")
+survDat <- hesCom[is.na(hesCom$survives_tplus1) != TRUE,]
+survMod <- glm(survives_tplus1 ~ basalArea_genet + Site + as.factor(age), data = survDat, family = "binomial")
 newData <- data.frame("basalArea_genet" <- seq(from = min(hesCom$basalArea_genet), 
                                                to = max(hesCom$basalArea_genet), 
                                                length.out = length(hesCom$basalArea_genet)))
@@ -31,9 +35,19 @@ newData$survives_tplus1 <-  predict(survMod,
         newdata = newData, 
         type = "response")
 
-plot(jitter(survives_tplus1) ~ basalArea_genet, data = hesCom)
-lines(survives_tplus1 ~ basalArea_genet, data = newData, col = "red")
+
 
 ggplot(data = hesCom, aes(x = basalArea_genet, y = survives_tplus1)) + 
-  geom_jitter(aes(col = Site), height = .01, alpha = .5) + 
-  geom_smooth(aes(col = Site), method = "glm", method.args = list(family = "binomial"))
+  geom_jitter(aes(col = as.factor(age)), height = .01, alpha = .5) + 
+  geom_smooth(aes(col = as.factor(age)),se = FALSE, method = "glm", method.args = list(family = "binomial"))
+
+# Prepare data for survival model -----------------------------------------
+## basic survival model glmer(survives_tplus1 ~ basalArea_genet + Site + age + (1|Year), family = "binomial")
+amod <- aov(survives_tplus1 ~ basalArea_genet, hesCom)
+
+
+
+# Prepare data for growth data --------------------------------------------
+## basic growth model: size_tplus1 ~ normal(alpha + Beta * basalArea_genet, sigma)
+
+## Stan model: 
